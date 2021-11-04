52d62ea7d8a7093227480d682b6ed1a72acf91ff
elvric <elvric.gtombert@gmail.com>
2021-11-04 12:48:22 +0100

    Settup the Firewall

--- firewall/setup_firewall.sh
+++ firewall/setup_firewall.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+#apt update
+#apt install -y iptables
+iptables -F
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+iptables -A FORWARD -s 172.17.0.0/16 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 443 -j ACCEPT
