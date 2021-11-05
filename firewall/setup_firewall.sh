4d45fc528e82d1fa4156ae58a48f58e5fc300d28
elvric <elvric.gtombert@gmail.com>
2021-11-05 16:25:35 +0100

    Updated the IP range for Francesco

--- firewall/setup_firewall.sh
+++ firewall/setup_firewall.sh
@@ -5,6 +5,6 @@ iptables -F
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
-iptables -A FORWARD -s 172.17.0.0/16 -j ACCEPT
+iptables -A FORWARD -s 172.27.0.0/16 -j ACCEPT
 iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
 iptables -A FORWARD -p tcp --dport 443 -j ACCEPT
