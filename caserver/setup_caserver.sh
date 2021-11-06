7b70d2127dab506ede7fcb3029a367c3be0481ff
elvric <elvric.gtombert@gmail.com>
2021-11-06 14:10:16 +0100

    Added python3 to CAserver

--- caserver/setup_caserver.sh
+++ caserver/setup_caserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get install nginx -y
+apt-get install nginx python3 -y
 cp /tmp/setup/nginx/nginx.conf /etc/nginx/sites-available/default
 mkdir /etc/nginx/ssl
 cp /tmp/setup/cert/* /etc/nginx/ssl
