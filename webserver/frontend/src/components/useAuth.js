072859c91aa161b053334fdc7b871ba796e26924
intx <francesco.intoci@epfl.ch>
2021-11-11 12:36:11 +0100

    working on fetching from backend

--- webserver/frontend/src/components/useAuth.js
+++ webserver/frontend/src/components/useAuth.js
@@ -1,5 +1,7 @@
+'use strict;'
 import * as React from "react";
-const axios = require('axios').default;
+const axios = require("axios").default;
+const https = require('https');
 
 const authContext = React.createContext();
 
@@ -8,41 +10,63 @@ function useAuth() {
 
   return {
     authed,
-    login(userID, password) {
-        //TO DO: setup fetch from api
+    login(user, password) {
+      const instance = axios.create({
+        httpsAgent: new https.Agent({
+          ca: [ fs.readFileSync('server-cert.pem') ], //root CA cert
+        }),
+        baseURL : 'https://webserver.imovies/api'
+      });
+      axios.post(
+        {
+          user: user,
+          password: password
+        });
       return new Promise((res) => {
         setAuthed(true);
         res();
       });
     },
-    loginWithCert(){
-      return
+    loginWithCert() {
+      const instance = axios.create({
+        httpsAgent: new https.Agent({
+          ca: [ fs.readFileSync('server-cert.pem') ], //root CA cert
+          key: fs.readFileSync('client-key.pem'),
+          cert: fs.readFileSync('client-cert.pem'),
+        }),
+        baseURL : 'https://webserver.imovies/api'
+      });
+      axios.post(
+        {
+          user: user,
+          password: password
+        });
+        return new Promise((res) => {
+          setAuthed(true);
+          res();
+        });
     },
     logout() {
       return new Promise((res) => {
         setAuthed(false);
         res();
       });
-    }
+    },
   };
 }
 
 export function AuthProvider({ children }) {
-    /*
+  /*
         Takes components children as argument
         Children can subscribe to the context that is passed as value props from Provider
         In this case is the context
     */
-    const auth = useAuth();
+  const auth = useAuth();
 
-    return (
-        <authContext.Provider value={auth}>
-          {children}
-        </authContext.Provider>
-    );
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 }
 
 export default function AuthConsumer() {
-    //The AuthConsumer must be enclosed with a AuthProvider
-    return React.useContext(authContext);
+  //The AuthConsumer must be enclosed with a AuthProvider
+  return React.useContext(authContext);
 }
