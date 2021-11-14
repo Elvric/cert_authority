4a6d44a3372f62147f8541ab0b555ad199c50fcc
elvric <elvric.gtombert@gmail.com>
2021-11-14 09:57:40 +0100

    Removed all openssl.cnf files (no longer needed)

--- caserver/setup_caserver.sh
+++ caserver/setup_caserver.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 apt update
-apt install nginx python3 python3-pip -y
-pip3 install uwsgi
+apt install nginx -y
 cp /tmp/setup/nginx/nginx.conf /etc/nginx/sites-available/default
 mkdir /etc/nginx/ssl
 cp /tmp/setup/cert/* /etc/nginx/ssl
+apt install python3 python3-pip -y
+pip3 install uwsgi
