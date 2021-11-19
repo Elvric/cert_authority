bdef788957e89fcd497d1fb6476af377c7558091
Lucas Rollet <lucas.rollet.69@gmail.com>
2021-11-19 19:58:41 +0100

    Update scripts to help VM configuration

--- database/setup_database.sh
+++ database/setup_database.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-echo -n 'CREATE DATABASE imovies;' | mysql -u root -pFiE5HF4xHOsPIL9n
-mysql -u root -pFiE5HF4xHOsPIL9n imovies < /tmp/setup/imovies_users.sql
-mysql -u root -pFiE5HF4xHOsPIL9n imovies < /tmp/setup/initdatabase.sql
+echo -n 'CREATE DATABASE imovies;' | sudo mysql -u root -pFiE5HF4xHOsPIL9n
+
+export MYSQL_ROOT_PASSWORD=FiE5HF4xHOsPIL9n
+
+mkdir /tmp/setup
+cp imovies_users.sql /tmp/setup/
+cp initdatabase.sql /tmp/setup/
+cp my.cnf /etc/mysql/conf.d
+cp cert/cacert.pem /etc/mysql/ssl/cacert.pem
+cp cert/db.pem /etc/mysql/ssl/db-cert.pem
+cp cert/db.key /etc/mysql/ssl/db-key.pem
+
+chown mysql /etc/mysql/ssl/db-cert.pem
+chown mysql /etc/mysql/ssl/db-key.pem
+chmod 644 /etc/mysql/ssl/cacert.pem
+chmod 644 /etc/mysql/ssl/db-cert.pem
+chmod 600 /etc/mysql/ssl/db-key.pem
+
+sudo mysql -u root -pFiE5HF4xHOsPIL9n imovies < /tmp/setup/imovies_users.sql
+sudo mysql -u root -pFiE5HF4xHOsPIL9n imovies < /tmp/setup/initdatabase.sql
--- webserver/setup_webserver.sh
+++ webserver/setup_webserver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 apt-get install nginx -y
-cp /tmp/setup/nginx/nginx.conf /etc/nginx/sites-available/default
+cp /webserver/nginx/nginx.conf /etc/nginx/sites-available/default
 mkdir /etc/nginx/ssl
-cp /tmp/setup/cert/* /etc/nginx/ssl
+cp -r /webserver/frontend/build/* /var/www/html/
+cp /webserver/cert/* /etc/nginx/ssl
