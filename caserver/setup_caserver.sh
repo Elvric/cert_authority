ff3ff6361e75765a705267b6e8d32034dce1b5b6
elvric <elvric.gtombert@gmail.com>
2021-11-21 15:24:57 +0100

    Added routing capabilities

--- caserver/setup_caserver.sh
+++ caserver/setup_caserver.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt install nginx net-tools -y
 sudo apt install libpcre3 libpcre3-dev -y
+sudo ip route add 172.26.0.0/24 via 172.27.0.254
 sudo cp caserver/nginx/nginx.conf /etc/nginx/sites-available/default
 sudo mkdir /etc/nginx/ssl
 sudo cp caserver/cert/* /etc/nginx/ssl
--- firewall/setup_firewall.sh
+++ firewall/setup_firewall.sh
@@ -2,10 +2,11 @@
 # TODO to be uncommented in final version
 sudo apt update
 sudo apt install net-tools -y
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
 #sudo apt install -y iptables
 #sudo iptables -P INPUT DROP
 #sudo iptables -P FORWARD DROP
 #sudo iptables -P OUTPUT DROP
-#sudo iptables -A FORWARD -s 172.27.0.0/16 -j ACCEPT
+#sudo iptables -A FORWARD -s 172.27.0.0/24 -j ACCEPT
 #sudo iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
 #sudo iptables -A FORWARD -s 172.26.0.2 -p tcp -dport 443 -j ACCEPT
