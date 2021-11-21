d4cbe0177d59bf4c1a208b5677b13ca941ea2f86
elvric <elvric.gtombert@gmail.com>
2021-11-21 15:02:03 +0100

    Corrected setup_webserver.sh

--- webserver/setup_webserver.sh
+++ webserver/setup_webserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt-get install net-tools nginx -y
-cp /webserver/nginx/nginx.conf /etc/nginx/sites-available/default
+cp webserver/nginx/nginx.conf /etc/nginx/sites-available/default
 mkdir /etc/nginx/ssl
-cp -r /webserver/frontend/build/* /var/www/html/
-cp /webserver/cert/* /etc/nginx/ssl
+cp -r webserver/frontend/build/* /var/www/html/
+cp webserver/cert/* /etc/nginx/ssl
