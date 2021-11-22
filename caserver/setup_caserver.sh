d5bdadfd46c86e840a0705e9258f2bcc6c72ead3
elvric <elvric.gtombert@gmail.com>
2021-11-22 16:33:51 +0100

    Updated nginx config files

--- caserver/setup_caserver.sh
+++ caserver/setup_caserver.sh
@@ -5,7 +5,6 @@ sudo apt install libpcre3 libpcre3-dev -y
 sudo cp caserver/nginx/nginx.conf /etc/nginx/sites-available/default
 sudo mkdir -p /etc/nginx/ssl
 sudo cp caserver/cert/* /etc/nginx/ssl
-sudo sudo cp caserver/intermediate/intermediate.pem /etc/nginx/ssl/intermediate.pem
 sudo apt install python3 python3-pip -y
 cd caserver/api
 pip3 install -r requirements.txt
