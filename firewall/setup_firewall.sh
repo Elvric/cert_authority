059011432d94cc35514400d47d90c27961f2460c
elvric <elvric.gtombert@gmail.com>
2021-11-22 11:39:53 +0100

    Corrected Firewall

--- firewall/setup_firewall.sh
+++ firewall/setup_firewall.sh
@@ -7,7 +7,8 @@ sudo sysctl -p
 #sudo apt install -y iptables
 #sudo iptables -P INPUT DROP
 #sudo iptables -P FORWARD DROP
-#sudo iptables -P OUTPUT DROP
+#sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+#sudo iptables -A OUTPUT ACCEPT
 #sudo iptables -A FORWARD -s 172.27.0.0/24 -j ACCEPT
 #sudo iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
-#sudo iptables -A FORWARD -s 172.26.0.2 -p tcp -dport 443 -j ACCEPT
+#sudo iptables -A FORWARD -s 172.26.0.2 -p tcp --dport 443 -j ACCEPT
