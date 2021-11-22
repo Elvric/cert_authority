0e96ce301a5d15c9e1d2f1669c32332ed7199b18
intx <francesco.intoci@epfl.ch>
2021-11-22 10:59:14 +0100

    fixed a bug in nginx for webserver

--- webserver/run.sh
+++ webserver/run.sh
@@ -1,4 +1,4 @@
 cp /webserver/nginx/nginx.conf /etc/nginx/sites-available/default
-service nginx restart
-npm run build --prefix /webserver/frontend
+#npm run build --prefix /webserver/frontend
 cp -r /webserver/frontend/build/* /var/www/html/
+service nginx restart
