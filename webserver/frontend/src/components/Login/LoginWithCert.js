5712c46e5d38aa4de153ed042f0662cf19c937cf
intx <francesco.intoci@epfl.ch>
2021-11-23 14:57:53 +0100

    changed frontend for login with cert

--- webserver/frontend/src/components/Login/LoginWithCert.js
+++ webserver/frontend/src/components/Login/LoginWithCert.js
@@ -21,63 +21,35 @@ forge.options.usePureJavascript = true;
 const asn1 = forge.asn1;
 
 export default function Login(props){
-    const [uploadFile, setUploadFile] = React.useState(null);
     const navigate = useNavigate();
     const AuthContext = AuthConsumer();
     const { state } = useLocation();
 
     const handleSubmit = async function (e) {
         e.preventDefault();
-        const dataArray = new FormData();
-        dataArray.append("uploadFile", uploadFile);
-        let cert = uploadFile[0]; //file object
-        var reader = new FileReader();
-        var fileByteArray = [];
-        reader.readAsArrayBuffer(cert);
-        reader.onloadend = function (evt) {
-            if (evt.target.readyState == FileReader.DONE) {
-                var arrayBuffer = evt.target.result;
-                var array = new Uint8Array(arrayBuffer);
-                for (var i = 0; i < array.length; i++) {
-                    fileByteArray.push(array[i]);
-                }
-                AuthContext.loginWithCert(fileByteArray).then(() => {
-                  if (state !== null){
-                    navigate(state);
-                  }
-                  else{
-                    navigate("/home");
-                  }
-                });
+        
+        AuthContext.loginWithCert().then(() => {
+            if (state !== null){
+                navigate(state);
             }
-        };
+            else{
+                navigate("/home");
+            }
+        });
     };
-
-    function validateForm() {
-        return (uploadFile !== null);
-    }
-    
     return (
         <div className="LoginPage">
             <Container fluid>
                 <Nav certificate={true}></Nav>
             </Container>
 
-            <div className="Login">            
-                <Form onSubmit={handleSubmit}>
-
-                <Form.Group controlId="formFile" className="mb-3">
-                    <Form.Label>Upload your certificate here</Form.Label>
-                    <Form.Control type="file" onChange={(e) => setUploadFile(e.target.files)}/>
-                </Form.Group>
+                        
+                
                 <div className="LoginButton">
-                    <Button block variant="success" size ="lg" type="submit" disabled={!validateForm()}>
+                    <Button block variant="success" size ="lg" type="submit" onClick={handleSubmit}>
                         Login
                     </Button>
                 </div>
-
-                </Form>
-            </div>
         </div>
       );
 }
