3d00a9b71689ba391500dde80f144b1839809ccb
intx <francesco.intoci@epfl.ch>
2021-11-25 11:34:05 +0100

    added bash to use openssl in backend

--- caserver/setup_caserver.sh
+++ caserver/setup_caserver.sh
@@ -6,12 +6,12 @@ sudo cp caserver/nginx/nginx.conf /etc/nginx/sites-available/default
 sudo mkdir -p /etc/nginx/ssl
 #sudo mkdir -p /etc/ca/intermediate
 sudo cp caserver/cert/* /etc/nginx/ssl
-#sudo cp caserver/intermediate/intermediate.pem /etc/nginx/ssl/intermediate.pem
-#sudo cp caserver/intermediate/ca-chain.pem /etc/nginx/ssl/ca-chain.pem
-#sudo cp caserver/intermediate/private/intermediate.key /etc/nginx/ssl/intermediate.key
-sudo chown www-data:www-data /etc/nginx/ssl/cacert.pem /etc/nginx/ssl/cakey.pem
+sudo cp caserver/intermediate/intermediate.pem /etc/nginx/ssl/intermediate.pem
+sudo cp caserver/intermediate/ca-chain.pem /etc/nginx/ssl/ca-chain.pem
+sudo cp caserver/intermediate/private/intermediate.key /etc/nginx/ssl/intermediate.key
+#sudo chown www-data:www-data /etc/nginx/ssl/cacert.pem /etc/nginx/ssl/cakey.pem
 #sudo cp caserver/intermediate /etc/ca/intermediate
-#sudo chown www-data:www-data /etc/nginx/ssl/intermediate.pem /etc/nginx/ssl/intermediate.key
+sudo chown www-data:www-data /etc/nginx/ssl/intermediate.pem /etc/nginx/ssl/intermediate.key
 #sudo chown www-data:www-data /etc/ca/intermediate
 #sudo chown www-data:www-data /etc/ca/intermediate/private/intermediate.key
 sudo apt install python3 python3-pip -y
