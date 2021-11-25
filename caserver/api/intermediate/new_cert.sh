2dc01b52e90cbe5e2c07c3089ace4045136e7820
intx <francesco.intoci@epfl.ch>
2021-11-25 11:53:17 +0100

    added ext to main

--- caserver/api/intermediate/new_cert.sh
+++ caserver/api/intermediate/new_cert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# use new_cert.sh <uid> <serial>
+# use new_cert.sh <serial> <uid>
 openssl genrsa -out ./certificates/client.key
 openssl req -config intermediate.cnf -key ./certificates/client.key -new -sha256 -out ./certificates/client.csr -subj "/C=CH/ST=VD/L=Lausanne/O=IMovies/OU=CA/CN=client/emailAddress=admin@imovies.ch"
 openssl ca -config intermediate.cnf -extensions usr_cert -days 365 -notext -md sha256 -in ./certificates/client.csr -out ./certificates/client.pem -batch
-openssl pkcs12 -export -in ./certificates/client.pem -inkey ./certificates/client.key -out ./certificates/client$1_$2.p12 -password pass:pass
+openssl pkcs12 -export -in ./certificates/client.pem -inkey ./certificates/client.key -out ./certificates/$1_$2.p12 -password pass:pass
 rm ./certificates/client.key
 rm ./certificates/client.pem
