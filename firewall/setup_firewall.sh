0614b658655fa932a1488344710eb604fe7b767a
elvric <elvric.gtombert@gmail.com>
2021-11-30 00:04:34 +0100

    Finished setting up the firewalls

--- firewall/setup_firewall.sh
+++ firewall/setup_firewall.sh
@@ -6,16 +6,17 @@ sudo sed -i -r "s/^#(net.ipv4.ip_forward=1)/\1/" /etc/sysctl.conf
 sudo sysctl -p
 sudo apt install -y iptables
 sudo iptables -F
-sudo iptables -P INPUT DROP
-sudo iptables -P FORWARD DROP
-sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-sudo iptables -A OUTPUT -j ACCEPT
-sudo iptables -A FORWARD -p tcp -s 172.27.0.2 -d 172.26.0.2 -j ACCEPT
-sudo iptables -A FORWARD -p tcp -s 172.27.0.0/24 --dport 22 -j ACCEPT
-sudo iptables -A FORWARD -p tcp --dport 22 -j ACCEPT
-sudo iptables -A FORWARD -s 172.26.0.2 -d 172.27.0.2 -p tcp --dport 443 -j ACCEPT
-sudo iptables -A FORWARD -s 172.26.0.2 -p tcp --dport 6514 -j ACCEPT
-sudo iptables -A INPUT -p icmp --icmp-type echo-request -j REJECT
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT DROP
+iptables -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -s 172.26.0.2/32 -d 172.27.0.2/32 -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -d 172.27.0.4/32 -p tcp -m tcp --dport 6514 -j ACCEPT
+iptables -A FORWARD -d 172.27.0.0/24 -p tcp -m tcp --dport 22 -j ACCEPT
+
 
 # Rsyslog
 cp ./firewall/rsyslog.conf /etc/rsyslog.conf
